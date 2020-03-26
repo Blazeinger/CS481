3c24db38167c7857de6e57dd19d74c101e480359
kgimlay <kgimlay@gmail.com>
2020-03-25 21:02:17 -0700

    Restructured it's face

---
+++ Assignment_5_Submission/Code/ast/ExpArrAccess.java
@@ -0,0 +1,22 @@
+package ast;
+
+public class ExpArrAccess extends Expression {
+    public Expression array;
+    public Expression index;
+    
+    public ExpArrAccess ( Position pos,
+			       Expression array,
+			       Expression index )
+    {
+	this.pos = pos;
+	this.array = array;
+	this.index = index;
+    }
+    
+    public <T> T accept(Visitor<T> visitor)
+    {
+        return visitor.visit(this);
+    }
+	
+
+}
---
+++ Assignment_5_Submission/Code/ast/StmExp.java
@@ -0,0 +1,19 @@
+package ast;
+
+
+public class StmExp extends Statement {
+    public Position pos;
+    public Expression exp;
+    
+    public StmExp (Position pos, Expression exp)
+    {
+	this.pos = pos;
+	this.exp = exp;
+	
+    }
+    
+    public <T> T accept(Visitor<T> visitor)
+    {
+        return visitor.visit(this);
+    }
+}
